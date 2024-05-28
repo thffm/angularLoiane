0be81f5288ab299068cd21d5f48469265e2ac6bd
thffm <thiagofmj16@outlook.com>
2024-05-28 19:10:42 -0300

    Table

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-courses',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  courses:Course[] = [
+    {_id:'1',name:'angular',category:'front-end'},
+  ];
+  displayedColumns = ['name','category',];
 
   constructor() { }
 
